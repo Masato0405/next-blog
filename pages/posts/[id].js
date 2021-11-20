0dd324de39af54e34e49c1a4775b18f0c1ae84bd
MasatoIso <04masato05@gmail.com>
2021-11-21 08:18:33 +0900

    render markdown content

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -3,7 +3,7 @@ import { getAllPostIds, getPostData } from "../../lib/posts";
 
 // idを元に必要な値を取得する
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id);
   return {
     props: {
       postData,
@@ -28,6 +28,8 @@ export default function Post({ postData }) {
       {postData.id}
       <br />
       {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   );
 }
